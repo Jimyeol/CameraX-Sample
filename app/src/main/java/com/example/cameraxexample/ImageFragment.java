faec06fea731219f4cfb7220868dbbf262195d1b
PARKKYOUNGJAE <economy@coinplug.com>
2020-11-10 15:23:56 +0900

    이미지 띄울 프래그먼트 추가

---
+++ app/src/main/java/com/example/cameraxexample/ImageFragment.java
@@ -0,0 +1,27 @@
+package com.example.cameraxexample;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ImageFragment extends Fragment {
+
+    public ImageFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_image, container, false);
+    }
+}
