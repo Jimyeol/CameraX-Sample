d6e262a23d7a039af628262632c0762203fa6db1
PARKKYOUNGJAE <economy@coinplug.com>
2020-11-10 15:46:47 +0900

    카메라 버튼 클릭스 토스트 출력 및 네비게이션을 통해 프래그먼트 이동

--- app/src/main/java/com/example/cameraxexample/CameraFragment.java
+++ app/src/main/java/com/example/cameraxexample/CameraFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class CameraFragment extends Fragment {
 
@@ -30,22 +31,26 @@ public class CameraFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        nav = Navigation.findNavController(view);
-        btnCapture = (ImageButton)view.findViewById(R.id.btnCapture);
-
-        btnCapture.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-        super.onViewCreated(view, savedInstanceState);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_camera, container, false);
+        return view;
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnCapture = (ImageButton)view.findViewById(R.id.btnCapture);
+        nav = Navigation.findNavController(view);
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.btnCapture:
+                        nav.navigate(R.id.imageFragment);
+                }
+            }
+        });
+
     }
 }
