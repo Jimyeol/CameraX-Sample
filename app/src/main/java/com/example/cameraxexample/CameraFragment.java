df162f56358a730174e8b1ff07cb76591ce33d00
PARKKYOUNGJAE <economy@coinplug.com>
2020-11-10 17:34:19 +0900

    카메라 프리뷰 출력

--- app/src/main/java/com/example/cameraxexample/CameraFragment.java
+++ app/src/main/java/com/example/cameraxexample/CameraFragment.java
@@ -1,13 +1,28 @@
 package com.example.cameraxexample;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.camera.core.AspectRatio;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +30,20 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class CameraFragment extends Fragment {
 
     NavController nav = null;
     ImageButton btnCapture = null;
+    PreviewView viewFinder = null;
+
+    private static final int REQUEST_CODE_CAMERA_PERMISSION = 0x0101;
+    private ExecutorService cameraExecutor = Executors.newSingleThreadExecutor();
 
     public CameraFragment() {
         // Required empty public constructor
@@ -28,6 +53,12 @@ public class CameraFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cameraExecutor.shutdown();
     }
 
     @Override
@@ -41,7 +72,18 @@ public class CameraFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         btnCapture = (ImageButton)view.findViewById(R.id.btnCapture);
+        viewFinder = (PreviewView)view.findViewById(R.id.view_finder);
         nav = Navigation.findNavController(view);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(getActivity(), new String[] { Manifest.permission.CAMERA}, REQUEST_CODE_CAMERA_PERMISSION);
+                return;
+            }
+        }
+
+        viewFinder.post(this::init);
+
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -53,4 +95,46 @@ public class CameraFragment extends Fragment {
         });
 
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE_CAMERA_PERMISSION) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                viewFinder.post(this::init);
+            }
+        }
+    }
+
+    private void init() {
+        final ListenableFuture<ProcessCameraProvider> cameraProviderListenableFuture = ProcessCameraProvider.getInstance(this.getContext());
+        cameraProviderListenableFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderListenableFuture.get();
+
+                DisplayMetrics metrics = new DisplayMetrics();
+                viewFinder.getDisplay().getRealMetrics(metrics);
+
+                Preview preview = new Preview.Builder()
+                        .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                        .build();
+
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+
+
+                preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
+
+                cameraProvider.unbindAll();
+
+                cameraProvider.bindToLifecycle(this.getActivity(), cameraSelector, preview);
+
+
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(getContext()));
+    }
+
 }
