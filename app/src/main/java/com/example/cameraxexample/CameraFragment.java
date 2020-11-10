e748bdccbaf1c3f444efbea3c8cf6b843b8466b6
PARKKYOUNGJAE <economy@coinplug.com>
2020-11-10 15:33:04 +0900

    카메라 버튼 레이아웃 수정 및 버튼 리스너 추가

--- app/src/main/java/com/example/cameraxexample/CameraFragment.java
+++ app/src/main/java/com/example/cameraxexample/CameraFragment.java
@@ -2,13 +2,22 @@ package com.example.cameraxexample;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class CameraFragment extends Fragment {
+
+    NavController nav = null;
+    ImageButton btnCapture = null;
 
     public CameraFragment() {
         // Required empty public constructor
@@ -17,6 +26,21 @@ public class CameraFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        nav = Navigation.findNavController(view);
+        btnCapture = (ImageButton)view.findViewById(R.id.btnCapture);
+
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        super.onViewCreated(view, savedInstanceState);
     }
 
     @Override
